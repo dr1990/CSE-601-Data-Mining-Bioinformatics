3ebff0af9e5d6cbef04b84645612d5292ad54032
dr1990 <deepak.ranjan1990@gmail.com>
2019-09-13 17:53:35 -0400

    PCA with plot

--- Project1/pca.py
+++ Project1/pca.py
@@ -26,26 +26,22 @@ def convert_to_array(file):
 def pca(data):
     # print(data)
     mean = np.mean(data, axis=0)
+    # print(mean)
+    # print("########")
     # print(data - mean)
 
     adj_data = data - mean
     cov = np.cov(adj_data.T)
-    # print(cov)
 
     eig_val, eig_vec = la.eig(cov)
-    print(eig_val)
-    print(eig_vec.shape)
 
-    # eig_val = eig_val[:2]
+    top_ind = eig_val.argsort()[-2:][::-1]
+    top_eig_vec = eig_vec[:, top_ind]
 
-    top_two_ind = eig_val.argsort()[-2:][::-1]
-    top_eig_vec = eig_vec[:, top_two_ind]
-    # print("#########")
-    print(top_eig_vec)
+    p = np.zeros([data.shape[0], top_eig_vec.shape[1]])
 
-    pca = np.dot(data, top_eig_vec)
-    print("#########")
-    print(pca)
+    p = np.dot(top_eig_vec.T, adj_data.T).T
+    return p
 
 
 def convert_to_num(data):
@@ -57,19 +53,52 @@ def convert_to_num(data):
     return out
 
 
+def plot_pca(pca, label):
+    np.random.seed(252354234)
+
+    distinct_lable = set([])
+
+    for x in label:
+        distinct_lable.add(x[0])
+
+    label_map = {}
+    i = 0
+    for x in distinct_lable:
+        label_map.update({x: i})
+        i += 1
+
+    colors = [plt.cm.jet(float(i) / max(label_map.values())) for i in label_map.values()]
+
+    p = np.append(pca, label, axis=1)
+    for key, value in label_map.items():
+        x = [float(k) for (t, k) in enumerate(p[:, 0]) if p[t, 2] == key]
+        y = [float(k) for (t, k) in enumerate(p[:, 1]) if p[t, 2] == key]
+        plt.scatter(x, y, c=colors[value], label=str(key))
+
+    plt.title(" Scatter Plot")
+    plt.legend()
+    plt.show()
+
+
 def main():
-    file = "pca_b.txt"
+    file = "pca_c.txt"
     lines = convert_to_array(file)
 
     r = lines.shape[0]
     c = lines.shape[1]
 
     data = lines[:, :-1]
-    label = lines[:, c - 1:c]
+    l = lines[:, c - 1:c]
+    label = np.array(l)
+
+    # print(label)
 
     data = convert_to_num(data)
-    pca(data)
-    plt.scatter(data[0], data[1])
+    p = pca(data)
+
+    # p = np.append(out, label, axis=1)
+    # print(p)
+    plot_pca(p, label)
 
 
 if __name__ == '__main__':
