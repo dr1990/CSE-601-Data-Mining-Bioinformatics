cb4ec966447d777835bf1154e24933b69b52cdda
Linus Castelino <castelinolinus@gmail.com>
2019-09-25 23:45:37 -0400

    PCA updated

--- Project1/Part1/pca.py
+++ Project1/Part1/pca.py
@@ -3,18 +3,8 @@ from numpy import linalg as la
 import matplotlib.pyplot as plt
 
 
-def getvalue(value):
-    # Remove "\n" from the last
-    if isinstance(value, str):
-        return value[:-1]
-    else:
-        return value
-
-
+# Transform file data into numpy array for processing
 def convert_to_array(file):
-    # with open(file, 'r') as f:
-    #     lines = [[getvalue(val) for val in l.split('\t')] for l in f]
-
     f = open(file, 'r')
     lines = f.read().splitlines()
     lines = [[val for val in l.split('\t')] for l in lines]
@@ -23,18 +13,21 @@ def convert_to_array(file):
     return lines
 
 
+# Calculating PCA
 def pca(data):
-    # print(data)
+    # Get mean along each dimension
     mean = np.mean(data, axis=0)
-    # print(mean)
-    # print("########")
-    # print(data - mean)
 
+    # Adjust the data around mean
     adj_data = data - mean
+
+    # find co-variance matrix
     cov = np.cov(adj_data.T)
 
+    # Get eigen-value and eigen-vector of the co-variance matrix
     eig_val, eig_vec = la.eig(cov)
 
+    # Pick top-two eigen-value and corresponding eigen-vector
     top_ind = eig_val.argsort()[-2:][::-1]
     top_eig_vec = eig_vec[:, top_ind]
 
@@ -44,6 +37,7 @@ def pca(data):
     return p
 
 
+# convert string data to number format
 def convert_to_num(data):
     s = np.array(data).shape
     out = np.zeros(s, dtype=float)
@@ -53,7 +47,7 @@ def convert_to_num(data):
     return out
 
 
-def plot_pca(pca, label):
+def plot_pca(pca, label, file):
     np.random.seed(252354234)
 
     distinct_lable = set([])
@@ -61,44 +55,44 @@ def plot_pca(pca, label):
     for x in label:
         distinct_lable.add(x[0])
 
+    # Color code the labels based on the distinct labels
     label_map = {}
     i = 0
     for x in distinct_lable:
         label_map.update({x: i})
         i += 1
 
-    colors = [plt.cm.jet(float(i) / max(label_map.values())) for i in label_map.values()]
+    # Color code different disease to unique number
+    color = [plt.cm.jet(float(val) / max(label_map.values())) for val in label_map.values()]
 
-    p = np.append(pca, label, axis=1)
     for key, value in label_map.items():
-        x = [float(k) for (t, k) in enumerate(p[:, 0]) if p[t, 2] == key]
-        y = [float(k) for (t, k) in enumerate(p[:, 1]) if p[t, 2] == key]
-        plt.scatter(x, y, c=colors[value], label=str(key))
+        x = [float(k) for (t, k) in enumerate(pca[:, 0]) if label[t] == key]
+        y = [float(k) for (t, k) in enumerate(pca[:, 1]) if label[t] == key]
+        plt.scatter(x, y, c=color[value], label=str(key))
 
-    plt.title(" Scatter Plot")
+    plt.title("Scatter Plot for " + file + ". Algorithm: PCA")
     plt.legend()
     plt.show()
 
 
 def main():
-    file = "pca_c.txt"
+    file = "pca_demo.txt"
     lines = convert_to_array(file)
 
-    r = lines.shape[0]
+    # Separate features and label
     c = lines.shape[1]
 
     data = lines[:, :-1]
     l = lines[:, c - 1:c]
     label = np.array(l)
 
-    # print(label)
-
     data = convert_to_num(data)
+
+    # Run PCA algorithm on the data
     p = pca(data)
 
-    # p = np.append(out, label, axis=1)
-    # print(p)
-    plot_pca(p, label)
+    # Scatter Plot of the of data in reduced dimension
+    plot_pca(p, label, file)
 
 
 if __name__ == '__main__':
