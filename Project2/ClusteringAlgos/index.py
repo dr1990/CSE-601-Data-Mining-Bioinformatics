58c8982e5745e75744b1a04c396280fbfbf84c19
Linus Castelino <castelinolinus@gmail.com>
2019-10-29 00:27:54 -0400

    Deleted redundant code/comments

--- Project2/ClusteringAlgos/index.py
+++ Project2/ClusteringAlgos/index.py
@@ -1,21 +1,4 @@
-import pandas as pd
 import numpy as np
-
-
-def readfile(filename):
-    data = pd.read_csv(filename, header=None, sep="\t")
-    data = np.array(data.values)
-    return data
-
-
-def write(data):
-    f = open('data.txt', 'a+')
-    shape = np.shape(data)
-    for x in range(shape[0]):
-        for y in range(shape[0]):
-            f.write(str(data[x][y]) + " ")
-        f.write("\n")
-    f.close()
 
 # Creates the incidence matrix indicating the similarity of cluster assignments to each datapoint
 def get_incidence_matrix(ground_truth, cluster_group):
@@ -28,7 +11,6 @@ def get_incidence_matrix(ground_truth, cluster_group):
                 incidence_matrix[row - 1][v[col] - 1] = 1
                 incidence_matrix[v[col] - 1][row - 1] = 1
 
-    # write(incidence_matrix)
     return incidence_matrix
 
 
@@ -58,29 +40,4 @@ def get_categories(im_a, im_b):
             x = im_a[i][j]
             y = im_b[i][j]
             categories[x][y] += 1
-
-            # if x == y and x == 1:
-            #     categories[1][1] += 1
-            # elif x == y and x == 0:
-            #     categories[0][0] += 1
-            # elif x != y and x == 1:
-            #     categories[1][0] += 1
-            # elif x != y and x == 0:
-            #     categories[0][1] += 1
     return categories
-
-
-# # filename = '../cho.txt'
-# filename = '../iyer.txt'
-# data = readfile(filename)
-# id = np.array(data[:, 0], dtype='int')
-# ground_truth = np.array(data[:, 1], dtype='int')
-# cluster_group = get_cluster_group(id, ground_truth)
-# incidence_matrix_gt = get_incidence_matrix(ground_truth, cluster_group)
-# categories = get_categories(incidence_matrix_gt, incidence_matrix_gt)
-
-# rand = (categories[0][0] + categories[1][1]) / np.sum(categories)
-# jaccard = categories[1][1] / (categories[1][0] + categories[0][1] + categories[1][1])
-
-# print("Rand: ", rand)
-# print("Jaccard: ", jaccard)
