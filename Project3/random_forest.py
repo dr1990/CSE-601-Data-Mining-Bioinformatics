5d742efa474b1a9ec4e974c4176831fb75bac557
dr1990 <deepak.ranjan1990@gmail.com>
2019-11-28 03:57:00 -0500

    Random Forest with variable feature selection.

--- Project3/random_forest.py
+++ Project3/random_forest.py
@@ -3,98 +3,103 @@ import numpy as np
 import statistics
 import random
 import time
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer, make_column_transformer
 from DecisionTree import DecisionTree, readfile, get_confusion_matrix
 from collections import Counter
 
 
 def readfile(filename):
-	data = pd.read_csv(filename, header=None, sep="\t")
-	data = np.array(data.values)
+    data = pd.read_csv(filename, header=None, sep="\t")
+    data = np.array(data.values)
 
-	str_ind = list()
+    str_ind = list()
 
-	for i in range(len(data[0])):
-		if isinstance(data[0][i], str):
-			str_ind.append(i)
+    for i in range(len(data[0])):
+        if isinstance(data[0][i], str):
+            str_ind.append(i)
 
-	for i in str_ind:
-		uniq = np.unique(data[:, i])
-		uniq_vals = list(range(len(uniq)))
-		str_val_dict = dict(zip(uniq, uniq_vals))
-		for j in range(len(data[:, i])):
-			data[j][i] = str_val_dict.get(data[j][i])
+    for i in str_ind:
+        uniq = np.unique(data[:, i])
+        uniq_vals = list(range(len(uniq)))
+        str_val_dict = dict(zip(uniq, uniq_vals))
+        for j in range(len(data[:, i])):
+            data[j][i] = str_val_dict.get(data[j][i])
 
-	return data
+    return data
+
 
 class RandomForest(object):
-	"""docstring for RandomForest"""
-	def __init__(self, N, m):
-		self.N = N
-		self.m = m
-		self.Trees = list()
-		for i in range(N):
-			self.Trees.append(DecisionTree())
-		print("Trees initialized")
-		# self.classified_labels = None
+    """docstring for RandomForest"""
 
-	def get_bootstrapped_data(self, data):
-		return data[np.random.choice(range(len(data)), len(data), replace=True)]
+    def __init__(self, N, m):
+        self.N = N
+        self.m = m
+        self.Trees = list()
+        for i in range(N):
+            self.Trees.append(DecisionTree(True, m))
+        print("Trees initialized")
 
-	def fit(self, data):
-		for i in range(self.N):
-			b_data = self.get_bootstrapped_data(data)
-			self.Trees[i].fit(b_data)
+    # self.classified_labels = None
 
-	def classify(self, data):
-		result = list()
-		for i in range(self.N):
-			self.Trees[i].classify(data)
-			result.append(self.Trees[i].classified_labels)
-		result = np.array(result, order='F').T
-		print(Counter(result[1]).most_common(1)[0][0])
-		self.classified_labels =  [ (Counter(result[i]).most_common(1))[0][0] for i in range(result.shape[0]) ]
+    def get_bootstrapped_data(self, data):
+        return data[np.random.choice(range(len(data)), len(data), replace=True)]
 
-	def accuracy_measures(self, truelabels):
-		cm = get_confusion_matrix(truelabels, self.classified_labels)
-		a = cm[0][0]
-		b = cm[0][1]
-		c = cm[1][0]
-		d = cm[1][1]
-		accuracy = (float)(a + d) / (float)(a + b + c + d)
-		precision = (float)(a) / (float)(a + c)
-		recall = (float)(a) / (float)(a + b)
-		f1_measure = (float)(2 * a) / (float)(2 * a + b + c)
-		return accuracy, precision, recall, f1_measure
+    def fit(self, data):
+        for i in range(self.N):
+            b_data = self.get_bootstrapped_data(data)
+            self.Trees[i].fit(b_data)
+
+    def classify(self, data):
+        result = list()
+        for i in range(self.N):
+            self.Trees[i].classify(data)
+            result.append(self.Trees[i].classified_labels)
+        result = np.array(result, order='F').T
+        # print(Counter(result[1]).most_common(1)[0][0])
+        self.classified_labels = [(Counter(result[i]).most_common(1))[0][0] for i in range(result.shape[0])]
+
+    def accuracy_measures(self, truelabels):
+        cm = get_confusion_matrix(truelabels, self.classified_labels)
+        a = cm[0][0]
+        b = cm[0][1]
+        c = cm[1][0]
+        d = cm[1][1]
+        accuracy = (float)(a + d) / (float)(a + b + c + d)
+        precision = (float)(a) / (float)(a + c)
+        recall = (float)(a) / (float)(a + b)
+        f1_measure = (float)(2 * a) / (float)(2 * a + b + c)
+        return accuracy, precision, recall, f1_measure
+
 
 def get_train_data(split_data, ind):
     return np.vstack([x for i, x in enumerate(split_data) if i != ind])
 
-def main(file, n):
-	data = readfile(file)
-	accuracy_list = list()
-	precision_list = list()
-	recall_list = list()
-	f1_measure_list = list()
-	# n-fold validation
-	split_data = np.array_split(data, n)
 
-	for i in range(n):
-		test = split_data[i]
-		train = get_train_data(split_data, i)
-		rf = RandomForest(5, .6)
-		rf.fit(train)
-		rf.classify(test)
-		accuracy, precision, recall, f1_measure = rf.accuracy_measures(test[:, -1])
-		accuracy_list.append(accuracy)
-		precision_list.append(precision)
-		recall_list.append(recall)
-		f1_measure_list.append(f1_measure)
-	print("Accuracy: ", sum(accuracy_list) / n)
-	print("Precision: ", sum(precision_list) / n)
-	print("Recall: ", sum(recall_list) / n)
-	print("F1-Measure: ", sum(f1_measure_list) / n)
+def main(file, n):
+    data = readfile(file)
+    accuracy_list = list()
+    precision_list = list()
+    recall_list = list()
+    f1_measure_list = list()
+    # n-fold validation
+    split_data = np.array_split(data, n)
+
+    for i in range(n):
+        test = split_data[i]
+        train = get_train_data(split_data, i)
+        m = int(train.shape[1] * 80 / 100)
+        rf = RandomForest(5, m)
+        rf.fit(train)
+        rf.classify(test)
+        accuracy, precision, recall, f1_measure = rf.accuracy_measures(test[:, -1])
+        accuracy_list.append(accuracy)
+        precision_list.append(precision)
+        recall_list.append(recall)
+        f1_measure_list.append(f1_measure)
+    print("Accuracy: ", sum(accuracy_list) / n)
+    print("Precision: ", sum(precision_list) / n)
+    print("Recall: ", sum(recall_list) / n)
+    print("F1-Measure: ", sum(f1_measure_list) / n)
+
 
 if __name__ == '__main__':
     file = 'project3_dataset1.txt'
